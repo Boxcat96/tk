1c60a22ac436dd84e620b9f3f02dc981511e746c
Boxcat <42795285+Boxcat96@users.noreply.github.com>
2024-07-16 22:51:51 +0900

    Add files via upload

--- main.m
+++ main.m
@@ -1,17 +1,25 @@
+% -- Add Dynare Path
 addpath c:\dynare\5.5\matlab
 
-dynare inf_zlb
+% -- Run Dynare
+dynare inf_lb
 
+% -- Save Result
 y      = Simulated_time_series.data(:,1);
 pi     = Simulated_time_series.data(:,2);
+IPI    = Simulated_time_series.data(:,3);
 epi    = Simulated_time_series.data(:,4);
 tau    = Simulated_time_series.data(:,5);
-r      = Simulated_time_series.data(:,6);
-pistar = Simulated_time_series.data(:,7);
+r_lb   = Simulated_time_series.data(:,7);
+data_tmp = [y pi epi tau r_lb];
 
-data_tmp = [y pi epi tau r pistar];
+% -- Export Result
+xlswrite('result/result_1.xlsx', data_tmp);
 
 % ショックの大きさ＝1/(1-rho)*変更後の定常状態のインフレ率(ベースラインは0.5)
-shocksize_epistar = 1/(1-rho)*0.5;
+terminal_rate = 0.5; % ターミナルレート
+
+disp('インフレ目標のショックサイズは')
+disp(1/(1-rho)*terminal_rate);
 
 close all;
