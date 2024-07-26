d7f415b61f4c4bd29ea54f0c8ec5c0c8fb4ce69e
Boxcat <42795285+Boxcat96@users.noreply.github.com>
2024-07-26 22:15:40 +0900

    Add files via upload

--- main.m
+++ main.m
@@ -4,20 +4,19 @@ addpath c:\dynare\5.5\matlab
 % -- Run Dynare
 dynare inf_lb
 
-% -- Save Result
-y      = Simulated_time_series.data(:,1);
-pi     = Simulated_time_series.data(:,2);
-IPI    = Simulated_time_series.data(:,3);
-epi    = Simulated_time_series.data(:,4);
-tau    = Simulated_time_series.data(:,5);
-r_lb   = Simulated_time_series.data(:,7);
-data_tmp = [y pi epi tau r_lb];
-
-% -- Export Result
-xlswrite('result/result_1.xlsx', data_tmp);
-
 % ショックの大きさ＝1/(1-rho)*変更後の定常状態のインフレ率(ベースラインは0.5)
 terminal_rate = 0.5; % ターミナルレート
+
+% -- Save Result
+y      = Simulated_time_series.data(:,1)/4; % 名目金利以外の他系列は年率なので４で割る
+pi     = Simulated_time_series.data(:,2) + (2-terminal_rate);
+epi    = Simulated_time_series.data(:,4) + (2-terminal_rate);
+tau    = Simulated_time_series.data(:,5) + (2-terminal_rate);
+r_lb   = Simulated_time_series.data(:,7);
+data_export = [y pi epi tau r_lb];
+
+% -- Export Result
+xlswrite('result/result_1.xlsx', data_export);
 
 disp('インフレ目標のショックサイズは')
 disp(1/(1-rho)*terminal_rate);
