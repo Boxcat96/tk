daedf3f4498360870fce91dd547314158ea01eff
Boxcat <42795285+Boxcat96@users.noreply.github.com>
2024-08-06 08:40:28 +0900

    Add files via upload

--- save_result.m
+++ save_result.m
@@ -1,18 +1,18 @@
 % -- Save Result
-if connect_flag == 0
-    y      = Simulated_time_series.data(:,1)/4; % 名目金利以外の他系列は年率なので４で割る
-    pi     = Simulated_time_series.data(:,2) + (2-terminal_rate);
-    epi    = Simulated_time_series.data(:,4) + (2-terminal_rate);
-    tau    = Simulated_time_series.data(:,5) + (2-terminal_rate);
-    r_lb   = Simulated_time_series.data(:,7);
-    data_export = [y pi epi tau r_lb];
 
-elseif connect_flag == 1
-    y      = Simulated_time_series.data(:,1)/4; % 名目金利以外の他系列は年率なので４で割る
-    pi     = Simulated_time_series.data(:,2);
-    epi    = Simulated_time_series.data(:,4);
-    tau    = Simulated_time_series.data(:,5);
-    r_lb   = Simulated_time_series.data(:,7);
-    data_tmp = [y pi epi tau r_lb];
-    data_export = data_tmp(2:end,:);
-end
+y_tmp      = Simulated_time_series.data(:,1)/4; % 名目金利以外の他系列は年率なので４で割る
+pi_tmp     = Simulated_time_series.data(:,2) + (2-terminal_rate);
+epi_tmp    = Simulated_time_series.data(:,4) + (2-terminal_rate);
+tau_tmp    = Simulated_time_series.data(:,5) + (2-terminal_rate);
+r_lb_tmp   = Simulated_time_series.data(:,7);
+
+y = y_tmp(1:100,:);
+pi = pi_tmp(1:100,:);
+epi = epi_tmp(1:100,:);
+tau = tau_tmp(1:100,:);
+r_lb = r_lb_tmp(1:100,:);
+
+data_export = [y pi epi tau r_lb];
+size_result = size(data_export,1);
+discount = beta*linspace(1,size_result,size_result);
+welfare = discount*(y.^2+(pi-2).^2)/size_result;
