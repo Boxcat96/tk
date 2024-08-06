7efaa9c8efe5b3c4d7aacb7f7711da3ecbca10e0
Boxcat <42795285+Boxcat96@users.noreply.github.com>
2024-08-07 08:34:04 +0900

    Add files via upload

--- main.m
+++ main.m
@@ -1,52 +1,34 @@
-%% ------------------------------------------------------------------------
 % -- Add Dynare Path
-clear all; clc; tic;
-% Set your own Dynare path
 addpath c:\dynare\5.5\matlab
 
-% -- option: calculate optimal Taylor rule or NOT
-CALC = 1; %(Do = 1, DONT = 0, Default = 1)
-
 % -- shock_size = terminal_rate*(rho+(1-rho)*phipi)/((1-rho)*phipi);
-terminal_rate = 1.5; 
+terminal_rate = 0.75; % ターミナルレート
 
-% -- set params conbination of Taylor Rule
-% lower bound, interval, upper bound 
-rhos =   0.0 : 0.1 : 0.9;
-phipis = 0.5 : 0.1 : 5.0;
-phiys =  0.1 : 0.1 : 1.0;
-taylor_set = table2array(combinations(rhos, phipis, phiys));
+% -- Run Dynare
+% ベンチマーク
+dynare inf_lb
+connect_flag = 0;
+save_result;
+xlswrite('result/benchmark.xlsx', data_export);
+save mat/inf_LB data_export
 
-% simlation number of attempts
-sim_num = 1000;
-shock_mean = 0.625;
-shock_stdev = 5;
-sim_period = 10;
+% 0.25bps利上げパターン
+dynare inf_lb_25hike
+connect_flag = 0;
+save_result;
+xlswrite('result/25hike.xlsx', data_export);
+save mat/25hike data_export
 
-%% ------------------------------------------------------------------------
+% 輸入物価ショック（プラス）
+dynare inf_lb_IPI_plus
+connect_flag = 1;
+save_result;
+xlswrite('result/IPI_plus.xlsx', data_export);
 
-% -- run for each case
-for simcase = 1:3
-    set_params;
-    
-    % calculate optimal taylor rule params
-    if CALC == 1
-        calc_optim;
-    elseif CALC == 0
-        if simcase == 1
-            load mat/result_optim_case_1.mat
-        elseif simcase == 2
-            load mat/result_optim_case_2.mat
-        elseif simcase == 3
-            load mat/result_optim_case_3.mat
-        else
-        end
-    else
-    end
-    
-    % stochastic simulation
-    rand_shock;
-end
+% 輸入物価ショック（マイナス）
+dynare inf_lb_IPI_minus
+connect_flag = 1;
+save_result;
+xlswrite('result/IPI_minus.xlsx', data_export);
 
-toc;
 close all;
