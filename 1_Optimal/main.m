1bb749a45618c1a3a4bc3bdc13f75fd61e56d7c4
Boxcat <42795285+Boxcat96@users.noreply.github.com>
2024-08-07 08:32:14 +0900

    Add files via upload

--- 1_Optimal/main.m
+++ 1_Optimal/main.m
@@ -4,20 +4,27 @@ clear all; clc; tic;
 addpath c:\dynare\5.5\matlab
 
 % -- option: calculate optimal Taylor rule or NOT
-CALC = 0; %(Do = 1, DONT = 0, Default = 1)
+CALC = 1; %(Do = 1, DONT = 0, Default = 1)
 
 % -- shock_size = terminal_rate*(rho+(1-rho)*phipi)/((1-rho)*phipi);
 terminal_rate = 1.5; 
 
-% -- parameter setting
+% -- set params conbination of Taylor Rule
+% lower bound, interval, upper bound 
+rhos =   0.0 : 0.1 : 0.9;
+phipis = 0.5 : 0.1 : 5.0;
+phiys =  0.1 : 0.1 : 1.0;
+taylor_set = table2array(combinations(rhos, phipis, phiys));
+
+% simlation number of attempts
+sim_num = 10;
+shock_mean = 0.625;
+shock_stdev = 5;
+sim_period = 10;
+
+% -- run for each case
 for simcase = 1:3
     set_params;
-    % -- set params conbination of Taylor Rule
-    % lower bound, interval, upper bound 
-    rhos =   0.0 : 0.1 : 0.9;
-    phipis = 0.5 : 0.1 : 5.0;
-    phiys =  0.1 : 0.1 : 1.0;
-    taylor_set = table2array(combinations(rhos, phipis, phiys));
     
     % calculate optimal taylor rule params
     if CALC == 1
@@ -33,13 +40,6 @@ for simcase = 1:3
         end
     else
     end
-
-
-    % simlation number of attempts
-    sim_num = 1000;
-    shock_mean = 0.625;
-    shock_stdev = 5;
-    sim_period = 10;
     
     % stochastic simulation
     rand_shock;
