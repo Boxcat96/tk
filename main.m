f1d3233e4d879bdd1a0a5561c7699821ad63cf0e
Boxcat <42795285+Boxcat96@users.noreply.github.com>
2024-08-08 07:25:27 +0900

    Add files via upload

--- main.m
+++ main.m
@@ -1,41 +1,34 @@
-%% ------------------------------------------------------------------------
 % -- Add Dynare Path
-clear all; clc; tic;
-% Set your own Dynare path
 addpath c:\dynare\5.5\matlab
 
-% -- option: calculate optimal Taylor rule or NOT
-CALC = 1; %(Do = 1, DONT = 0, Default = 1)
-
 % -- shock_size = terminal_rate*(rho+(1-rho)*phipi)/((1-rho)*phipi);
-terminal_rate = 1.5;
+terminal_rate = 0.75; % ターミナルレート
 
-% -- set params conbination of Taylor Rule
-% lower bound, interval, upper bound
-rhos =   0.0 : 0.2 : 0.6;
-phipis = 0.6 : 0.2 : 2.0;
-phiys =  0.2 : 0.2 : 1.0;
-taylor_set = table2array(combinations(rhos, phipis, phiys));
+% -- Run Dynare
+% ベンチマーク
+dynare inf_lb
+connect_flag = 0;
+save_result;
+xlswrite('result/benchmark.xlsx', data_export);
+save mat/inf_LB data_export
 
-% simlation number of attempts
-sim_num = 100;
-shock_mean = 0.625;
-shock_stdev = 5;
-sim_period = 10;
+% 0.25bps利上げパターン
+dynare inf_lb_25hike
+connect_flag = 0;
+save_result;
+xlswrite('result/25hike.xlsx', data_export);
+save mat/25hike data_export
 
-%% ------------------------------------------------------------------------
+% 輸入物価ショック（プラス）
+dynare inf_lb_IPI_plus
+connect_flag = 1;
+save_result;
+xlswrite('result/IPI_plus.xlsx', data_export);
 
-% -- run for each case
-for simcase = 1:3
-    % case 1 = ZLB on, Adapt Expectation on
-    % case 2 = ZLB on, Adapt Expectation off
-    % case 3 = ZLB off, Adapt Expectation off
-   
-    set_params;
-   
-    % stochastic simulation
-    rand_shock;
-end
+% 輸入物価ショック（マイナス）
+dynare inf_lb_IPI_minus
+connect_flag = 1;
+save_result;
+xlswrite('result/IPI_minus.xlsx', data_export);
 
-toc;
 close all;
